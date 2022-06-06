a092fb30d255f3e31990e90ee6f4708b48303438
Niged01 <nigedoberer@gmail.com>
2022-06-06 17:26:22 +0000

    created interface and submit function

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,21 +1,21 @@
 import React from 'react';
 import './App.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGoogle, faGithub } from '@fortawesome/free-brands-svg-icons'
-import { useForm} from 'react-hook-form'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGoogle, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { useForm} from 'react-hook-form';
 
 interface FormData {
   username: string
   password: string
-}
+};
 
 
 function App() {
-  const {registar, handleSubmit, errors } = useForm<FormData>();
+  const {register, handleSubmit, formState: { errors }} = useForm<FormData>();
 
   const onSubmit = handleSubmit((data) => {
     console.log(data)
-  })
+  });
 
 
   return (
@@ -27,22 +27,14 @@ function App() {
         <form action="" className='space-y-6' onSubmit={onSubmit}>
           <div>
             <label className='text-sm font-bold text-sky-600 pl-4'>Username</label>
-            <input 
-              ref={registar(
-                require: true
-                minLength: 5,
-                maxLength: 20,
-                validate: input => isUsername(input)
-              )}
+            <input {...register("username")}
               name="username" type="text" className='w-full p-2 border border-sky-700 rounded-3xl mt-1' />
               {errors.username && "Username is invalid"}
           </div>
           <div>
             <label className='text-sm font-bold text-sky-600 pl-4'>Password</label>
-            <input 
-              ref={registar()}
-              name="password"
-              type="text" className='w-full p-2 border border-sky-700 rounded-3xl mt-1' />
+            <input {...register("password")}
+              name="password" type="text" className='w-full p-2 border border-sky-700 rounded-3xl mt-1' />
           </div>
           <div>
             <button className='w-full p-2 bg-sky-400 hover:bg-sky-500 rounded-3xl text-stone-100'>Login</button>
